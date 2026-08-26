10a32073b71e186a53d23edf543d7619670fc63a
Whynot63 <5263261+Whynot63@users.noreply.github.com>
2026-08-27 01:35:16 +0300

    added aave addresses

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { AaveV3Ethereum } from '@bgd-labs/aave-address-book'
 import { publicClient } from './lib/client'
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     <main>
       <h1>cash-parking</h1>
       <p>{blockNumber === undefined ? 'Loading…' : `Mainnet block: ${blockNumber}`}</p>
+      <p>Aave v3 Pool: {AaveV3Ethereum.POOL}</p>
     </main>
   )
 }
